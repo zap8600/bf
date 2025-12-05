4fa97eca77b343465c2dd72fdcb2db0e313edda1
zap8600 <95487746+zap8600@users.noreply.github.com>
2025-12-05 14:28:45 +0000

    wip again

--- bf.c
+++ bf.c
@@ -2,6 +2,13 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define CELL uint8_t
+
 int main(int argc, char** argv) {
-    //
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
+
+    if(argc != 2) {
+        //
+    }
 }

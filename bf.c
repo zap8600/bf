56630c1a5a342bfc21d4e37d9ad63a606606c58c
zap8600 <95487746+zap8600@users.noreply.github.com>
2025-12-05 14:20:22 +0000

    wip

--- bf.c
+++ bf.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-    setbuf(stdout, NULL);
-    setbuf(stderr, NULL);
-
-    if(argc != 2) {
-        fprintf(stderr, "Usage: %s [brainf**k program]\n", argv[0]);
-        return 1;
-    }
-
-    FILE* bfprog = fopen(argv[1], "rb");
-    if(!bfprog) {
-        perror(argv[1]);
-        return 1;
-    }
+    //
 }
